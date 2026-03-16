c5f00855a8fa76931b35729586ecef59cae33ad2
SCHIEMSKY Mael <Mael.SCHIEMSKY@berger-levrault.com>
2026-03-16 11:53:21 +0100

    fix : performance issue

--- ids-jsonl_merger.sh
+++ ids-jsonl_merger.sh
@@ -11,10 +11,7 @@ cp $FLOW_SOURCES $OUTPUT_FILE
 
 # for each line the ids.properties, we took the id and the value
 # and we replace each instance of this id in the jsonl file with this value
-while read line; do
-    id=$(echo $line | cut -d'=' -f2)
-    val=$(echo $line | cut -d'=' -f1)
-    echo "id: " $id " val: " $val
-
-    sed -i "s~\"method\":$id}~\"method\":\"$val\"}~g" $OUTPUT_FILE
+while IFS='=' read -ra line; do
+    echo "id: ${line[1]} val: ${line[0]}"
+    sed -i "s|\"method\":${line[1]}}|\"method\":\"${line[0]}\"}|g" $OUTPUT_FILE
 done < $IDS_SOURCES
