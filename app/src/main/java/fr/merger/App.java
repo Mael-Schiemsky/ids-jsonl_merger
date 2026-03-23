5ef02e10a9db06997e93c731f92035a611c1e3de
SCHIEMSKY Mael <Mael.SCHIEMSKY@berger-levrault.com>
2026-03-23 10:35:25 +0100

    feat : merge all thread.jsonl in the java-agent output directory

--- app/src/main/java/fr/merger/App.java
+++ app/src/main/java/fr/merger/App.java
@@ -1,9 +1,11 @@
 package fr.merger;
 
+import java.io.File;
+
 public class App {
     public static void main(String[] args) {
-        if(args.length != 3) {
-            System.out.println("Usage: java -jar app.jar <ids.properties> <target.jsonl> <output_file.jsonl>");
+        if(args.length != 1) {
+            System.out.println("Usage: java -jar app.jar <directory_path where ids.properties and target.jsonl are located>");
             return;
         }
 
@@ -12,17 +14,29 @@ public class App {
         Mapper mapper = new Mapper();
         Writer writer = new Writer();
 
-        System.out.println("Reading files...");
-        String idsProperties = reader.readFile(args[0]);
-        String targetJsonl = reader.readFile(args[1]);
+        String directoryPath = args[0];
 
-        System.out.println("Adding ID on EXIT...");
-        targetJsonl = editor.addIdOnExit(targetJsonl);
+        File dir = new File(directoryPath);
+        File[] files = dir.listFiles();
 
-        System.out.println("Mapping data...");
-        String mappedData = mapper.map(idsProperties, targetJsonl);
+        String idsProperties = reader.readFile(directoryPath + "/ids.properties");
 
-        System.out.println("Writing output...");
-        writer.writeFile(mappedData, args[2]);
+        for (File file : files) {
+            String fileName = file.getName();
+
+            if(fileName.matches("thread-[0-9]*\\.jsonl")) {
+                System.out.println("Reading " + fileName + " file...");
+                String targetJsonl = reader.readFile(file.getPath());
+
+                System.out.println("Adding ID on EXIT in " + fileName + "...");
+                targetJsonl = editor.addIdOnExit(targetJsonl);
+
+                System.out.println("Mapping " + fileName + " data...");
+                String mappedData = mapper.map(idsProperties, targetJsonl);
+
+                System.out.println("Writing merged " + fileName + " output...");
+                writer.writeFile(mappedData, directoryPath + "/merged_" + fileName);
+            }
+        }
     }
 }
